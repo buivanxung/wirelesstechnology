01416be5642bd2567da6a82a7924aa8970ffed53
buivanxung <xungbv.uit@gmail.com>
2018-06-18 15:34:47 +0700

    ok

--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,25 @@
+#!/bin/bash
+#
+# load-test.sh
+#
+# Syntax:
+#     load-test.sh [url] [db] [duration] [users]
+#
+# By default this will perform a load test using Apache Bench of the
+# API URL, with 20 users for 10 seconds. That's enough to get a feel for
+# how responsive the system is. Longer tests can detect things like
+# memory and resource leaks. More intensive tests can detect things
+# like connection limits and operating system limits.
+#
+# Examples:
+#     load-test.sh
+#     load-test.sh http://localhost:3000/api/v1/todos todo 60 100
 
+# Use bash unofficial strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -eou pipefail
 IFS=$'\n\t'
 
-url=${1:-http://localhost:3000/api/v1/todos}
+url=${1:-http://localhost:5000/api/v1/todos}
 db=${2:-todo}
 duration=${3:-10}
 users=${4:-20}
